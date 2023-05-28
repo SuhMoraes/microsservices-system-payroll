70a8f209ee673cf61121218c6ca09c3bc838df98
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-05-28 00:32:12 -0300

    Change number port

--- user-api/src/main/java/com/hrsystem/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/hrsystem/userapi/UserApiApplication.java
@@ -1,15 +1,41 @@
 package com.hrsystem.userapi;
 
+import com.hrsystem.userapi.domain.User;
+import com.hrsystem.userapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.List;
+
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserApiApplication {
+public class UserApiApplication implements CommandLineRunner {
+		@Autowired
+		private UserRepository userRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserApiApplication.class, args);
+		public static void main(String[] args) {
+			SpringApplication.run(UserApiApplication.class, args);
+		}
+
+		@Override
+		public void run(String... args) throws Exception {
+			userRepository.saveAll(List.of(
+					new User(null, "Mayara", "may@mail.com","456", 35.5),
+					new User(null, "Maria Silva", "maria.silva@example.com", "password123", 32.98),
+					new User(null, "João Santos", "joao.santos@example.com", "password456", 42.55),
+					new User(null, "Ana Oliveira", "ana.oliveira@example.com", "password789", 29.50),
+					new User(null, "Pedro Souza", "pedro.souza@example.com", "passwordabc", 22.98),
+					new User(null, "Carla Pereira", "carla.pereira@example.com", "passworddef", 500),
+					new User(null, "Miguel Almeida", "miguel.almeida@example.com", "passwordghi", 600),
+					new User(null, "Sofia Costa", "sofia.costa@example.com", "passwordjkl", 700),
+					new User(null, "Antônio Rodrigues", "antonio.rodrigues@example.com", "passwordmno", 800),
+					new User(null, "Beatriz Ferreira", "beatriz.ferreira@example.com", "passwordpqr", 900),
+					new User(null, "Rafael Gomes", "rafael.gomes@example.com", "passwordstu", 1000),
+					new User(null, "Isabella Barbosa", "isabella.barbosa@example.com", "passwordvwx", 1100)
+			));
+
+		}
 	}
-
-}
--- user-api/src/main/java/com/hrsystem/userapi/service/impl/UserServiceImpl.java
+++ user-api/src/main/java/com/hrsystem/userapi/service/impl/UserServiceImpl.java
@@ -5,20 +5,25 @@ import com.hrsystem.userapi.repository.UserRepository;
 import com.hrsystem.userapi.service.UserService;
 import com.hrsystem.userapi.service.execeptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+    private final Environment env;
 
     @Override
     public User findById(Long id) {
+        log.info("USERL_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object no found"));
     }
 
