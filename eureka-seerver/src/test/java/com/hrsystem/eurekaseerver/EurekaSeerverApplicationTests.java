9c3d70ef2f0f40b97e0326a57cdda34c07ca48a8
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-05-27 22:19:16 -0300

    Eureka Server

---
+++ eureka-seerver/src/test/java/com/hrsystem/eurekaseerver/EurekaSeerverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrsystem.eurekaseerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSeerverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
