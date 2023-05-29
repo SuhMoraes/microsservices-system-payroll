165978bcae2f880de22a452c1352a098379509d1
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-05-28 22:00:21 -0300

    Test Payroll

--- payroll-api/src/test/java/com/hrsystem/payrollapi/PayrollApiApplicationTests.java
+++ payroll-api/src/test/java/com/hrsystem/payrollapi/PayrollApiApplicationTests.java
@@ -1,10 +1,12 @@
 package com.hrsystem.payrollapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PayrollApiApplicationTests {
+@SpringBootConfiguration
+class PaApiApplicationTests {
 
 	@Test
 	void contextLoads() {
